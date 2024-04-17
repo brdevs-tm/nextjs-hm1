b18464e0c9450547752c243f72f52b1795183655
Jahongir Hamidov <bright19.off@gmail.com>
2024-04-17 23:55:40 +0500

    main

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Article from "@/components/Article";
+import Article2 from "@/components/Article2";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Section from "@/components/Section";
+import Section2 from "@/components/Section2";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="space-y-10">
+      <Navbar />
+      <Hero />
+      <Section />
+      <Section2 />
+      <Article />
+      <Article2 />
+      <Footer />
+    </div>
+  );
+}
